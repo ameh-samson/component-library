ae9289e131e123de272bb32e898b5a1439d89791
Samson Ameh <amehsam69@gmail.com>
2023-12-25 15:46:53 +0100

    conditional icon fixed based on variant

--- src/components/Banners/Banner.jsx
+++ src/components/Banners/Banner.jsx
@@ -3,12 +3,27 @@ import classnames from "classnames";
 export default function Banner({ variant, message, title }) {
   // Declare component classes based on the variant
   const bannerClasses = classnames("banner", variant);
-  const iconClasses = classnames(
-    "fa-solid",
-    "fa-circle-xmark",
-    "banner-icons",
-    `${variant}-icon`
-  );
+
+  //   set different icon based on variant
+  let iconClass;
+  switch (variant) {
+    case "success":
+      iconClass = "fa-solid fa-circle-check";
+      break;
+    case "warning":
+      iconClass = "fa-sharp fa-solid fa-triangle-exclamation";
+      break;
+    case "error":
+      iconClass = "fa-solid fa-circle-xmark";
+      break;
+    case "neutral":
+      iconClass = "fa-solid fa-circle-info";
+      break;
+    default:
+      iconClass = "";
+  }
+
+  const iconClasses = classnames(iconClass, "banner-icons", `${variant}-icon`);
   const titleClasses = classnames("banner-title", `${variant}-title`);
   const descriptionClasses = classnames(
     "banner-description",
