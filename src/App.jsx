60bcbf0d63a06007e301bae57bd0a8cb33ca894f
Samson Ameh <amehsam69@gmail.com>
2023-12-25 20:08:20 +0100

    Card component library added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Badges from "./components/Badges/Badges";
 import Banner from "./components/Banners/Banner";
+import Card from "./components/Card/Card";
 export default function App() {
   return (
     <div>
-      <Banner
+      <Card
+        title="Easy Deployment"
+        description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et
+          magna sit morbi lobortis."
+      />
+    </div>
+  );
+}
+
+// <div>
+//   <Badges variant="red" badgeShape="pill">
+//     Buy Now
+//   </Badges>
+// </div>
+
+{
+  /* <Banner
         title="Congratulations!"
         variant="success"
         message=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
@@ -25,13 +42,5 @@ export default function App() {
         title="Update available"
         variant="neutral"
         message=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
-      />
-    </div>
-  );
+      /> */
 }
-
-// <div>
-//   <Badges variant="red" badgeShape="pill">
-//     Buy Now
-//   </Badges>
-// </div>
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function Card({ title, description }) {
+  const [isHovered, setIshovered] = useState(false);
+
+  function handleMouseOver() {
+    setIshovered(true);
+  }
+
+  function handleMouseOut() {
+    setIshovered(false);
+  }
+
+  return (
+    <div
+      className={`card-wrapper ${isHovered ? "hovered" : ""}`}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <img src="/CloudUpload.png" alt="upload icon" />
+      <div>
+        <h1 className="card-title">{title}</h1>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+}
